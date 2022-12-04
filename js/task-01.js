f17108ceef00c669f2c621afb29737d3c3963f03
Vova2580 <109892815+Vova2580@users.noreply.github.com>
2022-12-04 21:00:23 +0200

    Task 1 Done

--- js/task-01.js
+++ js/task-01.js
@@ -1,1 +1,11 @@
-const allCatetgories = document.querySelector
+const allCatetgories = document.querySelectorAll('.item');
+console.log(allCatetgories);
+console.log("Number of categories :", allCatetgories.length);
+
+
+
+const categoriesArray = [...allCatetgories].map(categories => { 
+    console.log(`Category: ${categories.children[0].textContent}`);
+
+    console.log(`Element: ${categories.children[1].children.length}`)
+});
