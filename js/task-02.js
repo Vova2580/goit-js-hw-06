1c05d969860552e8dc7682a9b28c925acade4ff4
Vova2580 <109892815+Vova2580@users.noreply.github.com>
2022-12-04 23:50:58 +0200

    Завдання 2 в процесі

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,18 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+const newListEl = document.createElement("li");
+
+newListEl.textContent = ingredients[0]
+newListEl.classList.add("item");
+
+
+
+const ingredientsList = document.querySelector("#ingredients");
+ingredientsList.appendChild(newListEl);
+
+console.log(ingredientsList)
+
+
