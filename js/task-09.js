461604d6bdd27c3f9911ed663958b4073699758b
Vova2580 <109892815+Vova2580@users.noreply.github.com>
2022-12-08 23:47:41 +0200

    Готова обов'язкова частина роботи (9 задач).

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,18 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changeBtn = document.querySelector(".change-color");
+
+const colorInfo = document.querySelector(".color");
+
+const randomColor = getRandomHexColor();
+
+changeBtn.addEventListener("click", colorChange);
+
+function colorChange(event) {
+  document.body.style.backgroundColor = randomColor;
+
+  colorInfo.textContent = randomColor;
+}
+
